dc921225a654c0f63ca6802be9a4c6bab78e7196
sash9696 <sahilchopra838@gmail.com>
2023-10-20 14:49:12 +0530

    array reverse c++

---
+++ c++/arrays/reverse_array.cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[], int n)
+{
+
+    int s = 0;
+    int e = n - 1;
+
+    while (s < e)
+    {
+        int temp = arr[s];
+
+        arr[s] = arr[e];
+        arr[e] = temp;
+
+        s++;
+        e--;
+    }
+}
+
+int main()
+{
+
+    int arr[] = {1, 2, 3, 4, 5, 10};
+
+    int n = sizeof(arr) / sizeof(int);
+
+    reverseArray(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    };
+    cout << endl;
+
+    return 0;
+}
+
+//Time complexity O(N)
